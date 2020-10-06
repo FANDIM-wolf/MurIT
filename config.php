2f6f82d5411c733fc05dbc24ec0fd8135e15ba50
Michael Shishov <63235817+FANDIM-wolf@users.noreply.github.com>
2020-10-06 17:36:44 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+$config =  array('title' =>  'Mur It', 
+    
+    'vk_url' => 'http://vk.com/m.shishov99',
+
+
+	'db'=>array(
+
+
+	'server' => '127.0.0.1',
+	'username' => 'root',
+	'password' => '',
+	'name' => 'mur' 
+)
+);
+
+require "db.php";
+
+?>
